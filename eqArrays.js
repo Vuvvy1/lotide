b6356f560c470bc25dbf524d362dbcea8b44636f
Hadley <sutherlandhadley@gmail.com>
2022-11-15 09:27:10 -0700

    checks arr

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(actual, expected){
+  for (let i = 0; i < actual.length; i++){
+    if (actual[i] !== expected[i]) return false;
+}
+return true
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
+/*
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays([1, 2, 3], [1, 2, 3]); // => should PASS
+*/
