30ce87b52ace6fea124bede48d42a0ea40a59786
Hadley <sutherlandhadley@gmail.com>
2022-11-16 15:41:57 -0700

    finished

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (letters) {
+  let results = {};
+  for (let letter of letters) {
+    if (letter !== " ") {
+      if (results[letter] === undefined) {
+        results[letter] = 0;
+      }
+      results[letter] += 1;
+    }
+  }
+  return results
+}
+console.log(countLetters("lighthouse in the house"));
+assertEqual(countLetters("lighthouse in the house").e, 3)
