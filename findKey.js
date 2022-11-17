4e3a03b20ee69ee98086658a9e659fffc30f0aa4
Hadley <sutherlandhadley@gmail.com>
2022-11-17 15:44:56 -0700

    new file

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(objectlist, check) {
+  const names = Object.keys(objectlist);
+  for (let name of names) {
+    if (check(objectlist[name])) {
+      return name;
+    }
+  }
+};
+
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
